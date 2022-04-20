e91f870f8ba3db82ba0cf14e5f2c2cb1b2786a06
Jarrod Roberson <jarrod@vertigrated.com>
2022-04-19 22:56:58 -0400

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,40 @@
+use std::{env, fs};
+use std::error::Error;
+use std::os::unix::fs::MetadataExt;
+use std::os::unix::fs::PermissionsExt;
+
+use chrono::prelude::{TimeZone, Utc};
+use chrono::SecondsFormat;
+use filetime::FileTime;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+struct File {
+    name: String,
+    size: u64,
+    permissions: u32,
+    last_modified: String,
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let current_dir = env::current_dir()?;
+
+    for entry in fs::read_dir(current_dir)? {
+        let entry = entry?;
+        let path = entry.path();
+        let metadata = fs::metadata(&path)?;
+        if metadata.is_file() {
+            let lmt = FileTime::from_last_modification_time(&metadata);
+            let file = File {
+                name: entry.file_name().into_string().unwrap(),
+                size: metadata.size(),
+                permissions: metadata.permissions().mode(),
+                last_modified: Utc.timestamp(lmt.seconds(), lmt.nanoseconds()).to_rfc3339_opts(SecondsFormat::Micros, true),
+            };
+            println!("{}", serde_json::to_string_pretty(&file)?);
+        }
+    }
+
+    Ok(())
+}
+
