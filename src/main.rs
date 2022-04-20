eb7f9d4664024aa41356d2f624ccc3ffd3e49a6c
Jarrod Roberson <jarrod@vertigrated.com>
2022-04-20 01:05:36 -0400

    adding command line argument parser and updating README.md

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
-use std::{env, fs};
+use std::{fs};
+use std::env::current_dir;
 use std::error::Error;
 use std::os::unix::fs::MetadataExt;
 use std::os::unix::fs::PermissionsExt;
+use std::path::PathBuf;
 
 use chrono::prelude::{TimeZone, Utc};
 use chrono::SecondsFormat;
+use clap::Parser;
 use filetime::FileTime;
 use serde::{Deserialize, Serialize};
 
@@ -22,8 +25,20 @@ struct File {
     last_modified: String,
 }
 
+#[derive(Parser)]
+struct Args {
+    #[clap(parse(from_os_str))]
+    path: Option<std::path::PathBuf>,
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
-    let current_dir = env::current_dir()?;
+    let args = Args::parse();
+    let dir = process_directory(args.path.unwrap_or(current_dir()?))?;
+    println!("{}", serde_json::to_string_pretty(&dir)?);
+    Ok(())
+}
+
+fn process_directory(current_dir: PathBuf) -> Result<Directory, Box<dyn Error>> {
     let mut root = Directory {
         name: current_dir.file_name().unwrap().to_os_string().into_string().unwrap(),
         contents: Vec::new(),
@@ -44,7 +59,6 @@ fn main() -> Result<(), Box<dyn Error>> {
             root.contents.push(file);
         }
     }
-    println!("{}", serde_json::to_string_pretty(&root)?);
-    Ok(())
+    Ok(root)
 }
 
